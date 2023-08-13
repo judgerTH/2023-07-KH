5650f05ff962f8fe93dc80dd70caf2e20825004f
MinheeJ <mhjeeeee15@gmail.com>
2023-08-13 20:28:57 +0900

    final project start init

---
+++ src/main/java/com/kh/app/FinalKhCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.kh.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalKhCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalKhCommunityApplication.class, args);
+	}
+
+}
