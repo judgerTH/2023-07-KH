935dcbd75c9f76ab85f1e7b652e7a29405198714
LeeJangJun1101 <wkdwnsdl123@naver.com>
2023-08-25 20:30:24 +0900

    오늘은 여기까지...

---
+++ src/main/java/com/kh/app/board/entity/Report.java
@@ -0,0 +1,27 @@
+package com.kh.app.board.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Report {
+	private int reportId;
+	private int postId;
+	private int commentId;
+	private int messageId;
+	private String reporterId;
+	private String attackerId;
+	private String reportContent;
+	private String reportType;
+	private LocalDateTime reportSendDate;
+	private String reportCheck;
+	private String reportAction;
+	
+}
