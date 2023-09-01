93f717198676aa5ba3228b765e163245bdd57186
dho9 <he7852@naver.com>
2023-09-01 15:34:06 +0900

    sql 수정~

--- src/main/java/com/kh/app/config/WebSecurityConfig.java
+++ src/main/java/com/kh/app/config/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-			.antMatchers("/", "/index.jsp").permitAll()
+			.antMatchers("/", "/index.jsp","/member/mailCheck").permitAll()
 			.antMatchers("/board/**").permitAll()
 			.antMatchers("/admin/**").hasAuthority("ADMIN")
 			.antMatchers("/board/*").permitAll()
