3922593ecae620bc3e6d74fdab26ea9f21b72fbf
LeeJangJun1101 <wkdwnsdl123@naver.com>
2023-09-01 20:34:12 +0900

    진짜끝

--- src/main/java/com/kh/app/board/repository/BoardRepository.java
+++ src/main/java/com/kh/app/board/repository/BoardRepository.java
@@ -294,7 +294,7 @@ public interface BoardRepository {
 	String findReceivedIdByCommentRef(int ref);
 
 
-	@Update(" update study set study_people = study_people+1 where study_id =#{studyId}")
+	@Update("update study set study_people = study_people+1 where study_id =#{studyId}")
 	int updateStudyCount(int studyId);
 	
 	@Select ("select * from study where study_id=#{studyId}")
@@ -315,7 +315,7 @@ public interface BoardRepository {
 	@Select("select member_id from study_info where study_id = #{studyId}")
 	List<StudyMemberId> findStudyMemberIdList(int studyId);
 	
-	@Update(" update study set study_people = study_people-1 where study_id =#{studyId}")
+	@Update("update study set study_people = study_people-1 where study_id =#{studyId}")
 	int minusStudyCount(int studyId);
 
 	@Select("select * from student where student_id=#{memberId}")
