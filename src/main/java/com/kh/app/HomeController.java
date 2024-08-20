4ac38a8cde05c32c6cb4d0ca466b0948e44102a1
judgerTH <disney1026dlxogus@gmail.com>
2024-08-20 14:09:02 +0900

    Fix HomeController to forward to JSP page

--- src/main/java/com/kh/app/HomeController.java
+++ src/main/java/com/kh/app/HomeController.java
@@ -6,11 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping
-	public String home() {
-		
-		
-		return "forward:/index.jsp";
-	}
+    @GetMapping("/")
+    public String home() {
+        return "index"; // JSP 파일은 src/main/webapp/WEB-INF/jsp/index.jsp 경로에 있어야 함
+    }
 }
-
